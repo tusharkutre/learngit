027462d6597ba3deed7405a23b207f440736a9df
tushar kutre <tusharkutre21@gmail.com>
2022-10-16 18:08:55 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum
+{
+   public static void main(String[] args)
+    {
+        int a=10;
+        int b=10;
+        int sum;
+
+        sum=a+b;
+
+        System.out.println(sum);
+    }
+}
